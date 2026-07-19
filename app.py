aa0370435d1b8a0009eb6819f15818b7fb4c63c7
maulikdudhat010-source <maulikdudhat010@gmail.com>
2026-07-19 09:24:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -385,9 +385,11 @@ if app_route == "dashboard":
 # ==========================================
 if app_route == "(1) Office Expense Master":
     st.header("🏢 Office Records & Accounting Ledger")
+    
     if st.button("⬅ Back To Main Dashboard", key="back_office"):
     go_dashboard()
     st.rerun()
+    
     is_editing_office = (st.session_state.edit_section == "Office" and st.session_state.edit_id is not None)
     edit_office_row = st.session_state.edit_data if is_editing_office else {}
     Token = st.session_state.form_reset_token
