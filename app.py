1f639a911aab85732b754cd4f7c51ef8e1a0e2c4
maulikdudhat010-source <maulikdudhat010@gmail.com>
2026-07-19 10:12:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -625,8 +625,6 @@ elif app_route == "(2) Home Expense Master":
 # ==========================================
 elif app_route == "(3) Operator Ledger & Production Desk":
     st.header("👷 Diamond Laser 4P Production Terminal")
-elif app_route == "(3) Operator Ledger & Production Desk":
-    st.header("👷 Diamond Laser 4P Production Terminal")
 
     if st.button("⬅ Back To Main Dashboard", key="back_production"):
         go_dashboard()
